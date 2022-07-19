07c07ddc91447a290c5723e2818f37a9e3ab34e5
Maju França <juliafkoch@gmail.com>
2022-07-19 17:07:21 -0300

    Classes
    Montando pratinhos

---
+++ ExercicioClasses/ExercicioClasses/src/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioClasses.src
+{
+    public class Prato
+    {
+        public Salada Salada { get; set; }
+        public Carboidrato Carboidrato { get; set; }
+        public Proteina Proteina { get; set; }
+
+        public Prato()
+        {
+
+        }
+
+        public Prato(Salada salada, Carboidrato carboidrato, Proteina proteina)
+        {
+            Salada = salada;
+            Carboidrato = carboidrato;
+            Proteina = proteina;
+        }
+        public override string ToString()
+        {
+            return $"O prato foi: {Salada}, {Carboidrato} e {Proteina}.";
+        }
+    }
+    public enum Salada
+    {
+        Tomate, Rúcula, Cenoura, Alface, Couve
+    }
+    public enum Carboidrato
+    {
+        Arroz, Macarrão, Quinoa
+    }
+    public enum Proteina
+    {
+        Carne, Frango, Ovo
+    }
+
+}
---
+++ ExercicioClasses/ExercicioClasses/src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ExercicioClasses.src;
+
+namespace ExercicioClasses.src
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prato _boaz = new Prato(Salada.Rúcula,Carboidrato.Arroz,Proteina.Carne);
+            Prato _maju = new Prato(Salada.Tomate, Carboidrato.Macarrão, Proteina.Frango);
+            Console.WriteLine(_boaz.ToString());
+            Console.WriteLine(_maju.ToString());
+        }
+    }
+}
